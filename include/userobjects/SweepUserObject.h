59e067d480ae8f4b7fd99782cb880ccfa7d2850f
Derek Gaston <derek.gaston@inl.gov>
2021-02-03 18:12:27 -0700

    Some object cleanup

--- include/userobjects/SweepUserObject.h
+++ include/userobjects/SweepUserObject.h
@@ -9,68 +9,26 @@
 
 #pragma once
 
-#include "libmesh/libmesh_config.h"
-
-#ifdef LIBMESH_HAVE_FPARSER
-
 #include "GeneralUserObject.h"
-#include "libmesh/fparser.hh"
 
 // Forward Declarations
-class Terminator;
+class SweepUserObject;
 
 template <>
-InputParameters validParams<Terminator>();
+InputParameters validParams<SweepUserObject>();
 
-/**
- * This Userobject requests termination of the current solve based on
- * the values of Postprocessors (and a logical expression testing them)
- *
- *                     <((((((\\\
- *                     /      . }\
- *                     ;--..--._|}
- *  (\                 '--/\--'  )
- *   \\                | '-'  :'|
- *    \\               . -==- .-|
- *     \\               \.__.'   \--._
- *     [\\          __.--|       //  _/'--.
- *     \ \\       .'-._ ('-----'/ __/      \
- *      \ \\     /   __>|      | '--.       |
- *       \ \\   |   \   |     /    /       /
- *        \ '\ /     \  |     |  _/       /
- *         \  \       \ |     | /        /
- *          \  \      \        /
- */
-class Terminator : public GeneralUserObject
+class SweepUserObject : public GeneralUserObject
 {
 public:
   static InputParameters validParams();
 
-  Terminator(const InputParameters & parameters);
+  SweepUserObject(const InputParameters & parameters);
 
   virtual void initialize() override {}
   virtual void execute() override;
   virtual void finalize() override {}
 
 protected:
-  const enum class FailMode { HARD, SOFT } _fail_mode;
-
-  /// Postprocessor names
-  std::vector<std::string> _pp_names;
-
-  // Number of postprocessors the expression depends on
-  unsigned int _pp_num;
-
-  /// Postprocessor values
-  std::vector<const PostprocessorValue *> _pp_values;
-
-  std::string _expression;
-
-  /// Fparser object
-  FunctionParserBase<Real> _fp;
-
-  /// Fparser parameter buffer
-  std::vector<Real> _params;
 };
 
 #endif // LIBMESH_HAVE_FPARSER
--- src/userobjects/SweepUserObject.C
+++ src/userobjects/SweepUserObject.C
@@ -7,77 +7,29 @@
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
-#include "libmesh/libmesh_config.h"
+#include "SweepUserObject.h"
 
-#ifdef LIBMESH_HAVE_FPARSER
+registerMooseObject("MooseApp", SweepUserObject);
 
-#include "Terminator.h"
-#include "MooseApp.h"
-#include "Executioner.h"
-
-registerMooseObject("MooseApp", Terminator);
-
-defineLegacyParams(Terminator);
+defineLegacyParams(SweepUserObject);
 
 InputParameters
-Terminator::validParams()
+SweepUserObject::validParams()
 {
   InputParameters params = GeneralUserObject::validParams();
-  params.addClassDescription("Requests termination of the current solve based on the values of "
-                             "Postprocessor value(s) via a logical expression.");
-  params.addRequiredCustomTypeParam<std::string>(
-      "expression",
-      "FunctionExpression",
-      "FParser expression to process Postprocessor values into a boolean value. "
-      "Termination of the simulation occurs when this returns true.");
-  MooseEnum failModeOption("HARD SOFT", "HARD");
-  params.addParam<MooseEnum>("fail_mode",
-                             failModeOption,
-                             "Abort entire simulation (HARD) or just the current timestep (SOFT).");
+  params.addClassDescription("Performs a parallel sweep");
+
   return params;
 }
 
-Terminator::Terminator(const InputParameters & parameters)
-  : GeneralUserObject(parameters),
-    _fail_mode(getParam<MooseEnum>("fail_mode").getEnum<FailMode>()),
-    _pp_names(),
-    _pp_values(),
-    _expression(getParam<std::string>("expression")),
-    _fp()
+SweepUserObject::SweepUserObject(const InputParameters & parameters)
+  : GeneralUserObject(parameters)
 {
-  // build the expression object
-  if (_fp.ParseAndDeduceVariables(_expression, _pp_names) >= 0)
-    mooseError(std::string("Invalid function\n" + _expression + "\nin Terminator.\n") +
-               _fp.ErrorMsg());
-
-  _pp_num = _pp_names.size();
-  _pp_values.resize(_pp_num);
-
-  // get all necessary postprocessors
-  for (unsigned int i = 0; i < _pp_num; ++i)
-    _pp_values[i] = &getPostprocessorValueByName(_pp_names[i]);
-
-  _params.resize(_pp_num);
 }
 
 void
-Terminator::execute()
+SweepUserObject::execute()
 {
-  // copy current Postprocessor values into the FParser parameter buffer
-  for (unsigned int i = 0; i < _pp_num; ++i)
-    _params[i] = *(_pp_values[i]);
-
-  // request termination of the run or timestep in case the expression evaluates to true
-  if (_fp.Eval(_params.data()) != 0)
-  {
-    if (_fail_mode == FailMode::HARD)
-      _fe_problem.terminateSolve();
-    else
-    {
-      _console << name() << " is marking the current solve step as failed.\n";
-      getMooseApp().getExecutioner()->picardSolve().failStep();
-    }
-  }
 }
 
 #endif
